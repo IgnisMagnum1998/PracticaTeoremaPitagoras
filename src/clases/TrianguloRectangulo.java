fc64658b2796691b2e10275c5ef8130da1ffa0dc
IgnisMagnum1998 <danieruhimeno1@gmail.com>
2025-05-20 16:57:38 +0200

    tercer commit

--- src/clases/TrianguloRectangulo.java
+++ src/clases/TrianguloRectangulo.java
@@ -1,14 +1,26 @@
 package clases;
 
 public class TrianguloRectangulo {
+	/**
+	 * Creo mis variables de los dos catetos a y b y la hipotenusa c
+	 */
 	double a;
 	double b;
 	double c;
+	/**
+	 * Constructor donde dos de los catetos son 1 y la hipotenusa se calcula automáticamente.
+	 */
 	public TrianguloRectangulo() {
 		this.a = 1;
 		this.b = 1;
-		this.c= (a+b)*Math.sqrt(2);
+		this.c= hipotenusa();
 	}
+	/**
+	 * Constructor donde inserto dos catetos y recibo la hipotenusa. Ninguno de los dos catetos debe ser menor o igual a 0.
+	 * En tal caso, saltará una excepción.
+	 * @param a cateto 1
+	 * @param b cateto 2
+	 */
 	public TrianguloRectangulo(double a, double b) {
 
 		try {
@@ -23,37 +35,65 @@ public class TrianguloRectangulo {
 			System.out.println(e.getMessage());
 		}
 	}
+	/**
+	 * @return area devuelve el 
+	 * area
+	 */
 	public double area() {
 		double area;
 		area=(this.a*this.b)/2;
 		return area;
 	}
+	/**
+	 * @return hipotenusa devuelve la hipotenusa
+	 */
 	public double hipotenusa() {
 		double hipotenusa;
 		hipotenusa= (this.a+this.b)*Math.sqrt(2);
 		return hipotenusa;
 	}
+	/**
+	 * @return perimetro devuelve el perimetro
+	 */
 	public double perimetro() {
 		double hip=hipotenusa();
 		double perimetro;
 		perimetro=this.a+this.b+hip;
 		return perimetro;
 	}
+	/**
+	 * @return this.a devuelve el cateto 1
+	 */
 	public double getA() {
-		return a;
+		return this.a;
 	}
+	/**
+	 * @param a le asigno el valor al cateto 1
+	 */
 	public void setA(double a) {
 		this.a = a;
 	}
+	/**
+	 * @return b devuelve el cateto 2
+	 */
 	public double getB() {
 		return b;
 	}
+	/**
+	 * @param b le asigno el valor al cateto 2
+	 */
 	public void setB(double b) {
 		this.b = b;
 	}
+	/**
+	 * @return c devuelvo la hipotenusa
+	 */
 	public double getC() {
 		return c;
 	}
+	/**
+	 * @param c le asigno el valor a la hipotenusa
+	 */
 	public void setC(double c) {
 		this.c = c;
 	}
